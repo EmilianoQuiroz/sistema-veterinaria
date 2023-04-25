7af900f8931ba98bf8f15c5516339fc4ff00c6ee
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-25 12:05:53 -0300

    Utilizacion del Hook useState en el proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
+// Importacion de los Hooks al formulario
+import {useState, useEffect} from 'react'
 // Creamos nuestro functional component
 function Header() {
+    /**
+     * Como buena practica el State debe ser declarado en la parte
+     * superior de nuestra funcion antes del return 
+     */
+    const [nombre,setNombre] = useState('');
+    setNombre('Hook')
+    console.log(nombre);
 
     return(
         <>
