5ea2c8c4d846cb4998f577a48f742108692b1e49
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-09 14:40:31 -0300

    Mostrar textos de manera condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -6,23 +6,32 @@ const ListadoPacientes = ({ pacientes }) => {
 
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-
-            <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
-            <p className="text-xl mt-5 mb-10 text-center">Administra tus {''}
-                <span className="text-red-500 font-bold">Pacientes y Turnos</span>
-            </p>
-
-            {/** Mapeo de pacientes */}
-            { pacientes.map( paciente => 
-                    /** El return esta implicito */
-                    /** Mostramos el listado de pacientes en pantalla */
-                    <Paciente 
-                        key = {paciente.id}
-                        paciente = {paciente}
-                    />
-                )}
-
-
+            {/** Si no hay pacientes mostrar el siguiente mensaje*/}
+            {pacientes && pacientes.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">Administra tus {''}
+                        <span className="text-red-500 font-bold">Pacientes y Turnos</span>
+                    </p>
+        
+                    {/** Mapeo de pacientes */}
+                    { pacientes.map( paciente => 
+                            /** El return esta implicito */
+                            /** Mostramos el listado de pacientes en pantalla */
+                            <Paciente 
+                                key = {paciente.id}
+                                paciente = {paciente}
+                            />
+                        )}
+                </>
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">Agrega a tus pacientes {''}
+                        <span className="text-red-500 font-bold">y apareceran aqui</span>
+                    </p>
+                </>
+            )}
         </div>
     );
 }
