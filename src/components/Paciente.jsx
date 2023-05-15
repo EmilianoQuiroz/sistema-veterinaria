7f1fad1b7e305c16a700521a6eb55186f8cf838a
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-15 15:27:14 -0300

    Integracion de Sweet Alert 2

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
 
     {/** Desestructuracion */}
@@ -5,12 +6,34 @@ const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
 
     // Funcion de confirmacion en caso de querer eliminar un paciente 
     const handleEliminar = () => {
-        const respuesta = confirm('¿Deseas eliminar este paciente?');
+        /**
+         * Usamos la libreria de Sweet Alert 2
+         * esta mostrara un cartel en el que se le pregunta al usuario
+         * si desea eliminar al paciente, en caso de la respuesta sea 
+         * canselar el paciente sigue en la lista, en caso de que la 
+         * respuesta sea confirmar el paciente sera eliminar de los 
+         * registros
+         */
+        Swal.fire({
+            title: '¿Desea eliminar este paciente?',
+            text: "El paciente sera eliminado del sistema",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText:'Cancelar',
+            confirmButtonText: 'Confirmar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                eliminarPaciente(id)
+              Swal.fire(
+                'Paciente eliminado',
+                'El paciente se elimino del sistema',
+                'success'
+              )
+            }
+          })
 
-        // Si la respuesta es true entonces eliminamos el paciente
-        if(respuesta) {
-            eliminarPaciente(id)
-        }
     }
 
   return (
